92ad2f7dd04b5ef613c6a048fc5e2f5a7a0c0cb2
Stefany Cristine <143985573+stefanycristine@users.noreply.github.com>
2024-06-03 21:10:44 -0300

    feat: tipos de dados em java (#1)

---
+++ TipoDado.java
@@ -0,0 +1,18 @@
+public class TipoDado {
+  
+  public static void main(String[] args) {
+    //demonstração de alguns tipos de dados
+    int numero;
+    float decimalCurto;
+    double decimalLongo;
+    char caracter;
+    String cadeiaCaracteres;
+
+    numero = 10;
+    decimalCurto = 10.5f; //float number
+    decimalLongo = 20.5;
+    caracter = 'A';
+    cadeiaCaracteres = "** Hello **";
+
+  }
+}
